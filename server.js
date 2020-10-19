504f19992ba189e4eeecd9bf38dac7b83c8f4f73
segun flexible <flexibleayodele@gmail.com>
2020-10-19 13:40:56 +0100

    setup app for heroku

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const todoController = require("./controllers/todoController")
 //Telling Node to use EJS
 
 app.set("view engine","ejs");
-app.use(express.static("./public"))
+app.use(express.static("public"))
 //Fire The Controller
 todoController(app)
 
 
-app.listen(8080)
+app.listen(process.env.PORT || 8080)
